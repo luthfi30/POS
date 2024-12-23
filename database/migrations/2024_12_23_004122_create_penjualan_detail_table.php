6629a42f2eb4ee010b66f4af61565e3d8c3b9f23
luthfi30 <lurrah30@gmail.com>
2024-12-23 07:56:22 +0700

    add migration

---
+++ database/migrations/2024_12_23_004122_create_penjualan_detail_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penjualan_detail', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_penjualan');
+            $table->integer('id_produk');
+            $table->integer('jumlah');
+            $table->integer('harga_jual');
+            $table->integer('subtotal');
+            $table->tinyInteger('diskon')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualan_detail');
+    }
+};
